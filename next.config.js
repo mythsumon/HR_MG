7a5606d2b31e3972aadde2d88bd6156471fd4f0d
Myt H Mon <mytmon00@gmail.com>
2025-12-08 16:33:05 +0630

    Update next.config.js to use remotePatterns instead of domains to fix deprecation warning

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3001',
+      },
+    ],
   },
   outputFileTracingRoot: __dirname,
   output: 'standalone',
